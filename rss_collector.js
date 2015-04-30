e984070531284fa19f462d631376172433cc772e
ufuk <ufukkocolu@conceptimago.com>
2015-04-30 16:19:51 +0300

    Rss collector added.

--- rss_collector.js
+++ rss_collector.js
@@ -3,13 +3,13 @@ var http = require('http');
 var parseString = require('xml2js').parseString
 var util = require('util')
 
-var __USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
-
 function RssCollector(options) {
+	var __USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+
 	if(!options.url)
 		throw new Error('Missing url')
 	collector.call(this, options)
-
+	
 	this.url = options.url
 	this.positionColumn = 'lastBuildDate'
 	var self = this
